9588459d82fa4f7123e896b48ae852ee1c0d9726
Nazimov Aleksey I <numamo@yandex.ru>
2021-12-23 19:41:16 +0000

    Dev lesson #3

--- 3_users/adding_sudo.sh
+++ 3_users/adding_sudo.sh
@@ -6,10 +6,18 @@ sudo cat /etc/passwd | grep "super_apt" > 1_creating_new_user.log
 
 # Adding sudo:-------------------------------------------------------
 echo "  --> Adding sudo"
-super_apt ALL=(ALL:ALL) NOPASSWD: /usr/bin/apt update
+sudo echo "super_apt ALL=(ALL) NOPASSWD: /usr/bin/apt update" > /etc/sudoers.d/super_apt
+
 
 # Checking user priviledges:-----------------------------------------
 echo "  --> Checking the root command"
+set user "alex"
+set password "alex19121988"
 su super_apt
 apt update > 2_update.log
+su "$user"
+expect "password: "
+send "$password"
+sudo deluser super_apt --remove-all-files
+
 
--- 3_users/create_remove_user.sh
+++ 3_users/create_remove_user.sh
@@ -1,5 +1,8 @@
+# Creating technical user without home:-------------------------------
 useradd test_user -M -N
 cat /etc/passwd | grep "test_user"
+
+# Deleting technical user:--------------------------------------------
 deluser test_user --remove-all-files
 cat /etc/passwd | grep "test_user"
 
